b6e938751f5ee3f3d696f2b32e01b6f9bf58b4ec
= <=>
2020-11-10 23:02:55 +0800

    new screens

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./screens/Main.js";
+import Registered from "./screens/Registered.js";
+import Completed from "./screens/Completed.js";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={Main} />
-        {/* <Route exact path="/numberEntered" component={} />
-        <Route exact path="/notified" component={} /> */}
+        <Route exact path="/registered" component={Registered} />
+        <Route exact path="/completed" component={Completed} />
       </Router>
     </div>
   );
