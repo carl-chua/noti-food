aa92158c1d527fffd2c1046bd39db0f62c84e255
= <=>
2020-11-10 22:58:10 +0800

    Added screens

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import logo from "./logo.svg";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import CallModal from "./components/CallModal";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Button from "react-bootstrap/Button";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import InformationCard from "./components/InformationCard";
+import Main from "./screens/Main.js";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <InformationCard />
-      <Footer />
-
-      {/*
       <Router>
-        <Route exact path="/" component={} />
-        <Route exact path="/numberEntered" component={} />
-        <Route exact path="/notified" component={} />
+        <Route exact path="/" component={Main} />
+        {/* <Route exact path="/numberEntered" component={} />
+        <Route exact path="/notified" component={} /> */}
       </Router>
-      <Footer />
-      */}
     </div>
   );
 }
---
+++ src/screens/Main.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import InformationCard from "../components/InformationCard";
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+      <InformationCard />
+      <Footer />
+    </div>
+  );
+}
