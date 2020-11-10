a08dba43a8bc4aa5757b8df1e541504a244d7b39
MaxChan97 <42763925+MaxChan97@users.noreply.github.com>
2020-11-10 22:36:33 +0800

    started on information card

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import "./App.css";
 import CallModal from "./components/CallModal";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Button from "react-bootstrap/Button";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import InformationCard from "./components/InformationCard";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
+      <InformationCard />
+      {/*
       <Router>
-        <Route exact path="/" component={CallModal} />
-        {/* <Route exact path="/numberEntered" component={} />
-        <Route exact path="/notified" component={} /> */}
+        <Route exact path="/" component={} />
+        <Route exact path="/numberEntered" component={} />
+        <Route exact path="/notified" component={} />
       </Router>
       <Footer />
+      */}
     </div>
   );
 }
---
+++ src/components/InformationCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import moment from "moment";
+import {
+  TelephoneFill,
+  SignpostSplitFill,
+  ChatLeftDotsFill,
+} from "react-bootstrap-icons";
+
+function InformationCard() {
+  const now = moment().format("DD MMM YYYY, h:mm a");
+
+  return (
+    <div className="InformationCard">
+      <h5>Now preparing...</h5>
+      <h1 className="OrderNumber">#241</h1>
+      <h5>{now}</h5>
+      <div className="RestaurantName">
+        <h1>Hua Bee Bak Chor Mee</h1>
+      </div>
+      <div className="row" style={{ justifyContent: "center" }}>
+        <div style={{ marginRight: "3px" }}>
+          <TelephoneFill size="38px" />
+          <p>Call</p>
+        </div>
+        <div style={{ marginLeft: "3px" }}>
+          <SignpostSplitFill size="38px" />
+          <p>Directions</p>
+        </div>
+      </div>
+      <div className="row" style={{ justifyContent: "center" }}>
+        <ChatLeftDotsFill />
+        <h4>Get notified through SMS!</h4>
+      </div>
+      <div className="row" style={{ justifyContent: "center" }}>
+        <input className="NumberInput" placeholder="e.g. 9123 4567" />
+        <button className="GoButton">Go</button>
+      </div>
+    </div>
+  );
+}
+
+export default InformationCard;
